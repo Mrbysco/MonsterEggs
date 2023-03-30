77b1ce80a53e13859829ee439968b93c3c4209ce
Mrbysco <Mrbysco@users.noreply.github.com>
2023-03-30 17:03:04 +0200

    Use `DatapackBuiltinEntriesProvider` instead of `JsonCodecProvider.forDatapackRegistry`

---
+++ src/main/java/com/mrbysco/monstereggs/datagen/MonsterBiomeModifiers.java
@@ -0,0 +1,91 @@
+package com.mrbysco.monstereggs.datagen;
+
+import com.mrbysco.monstereggs.MonsterEggs;
+import com.mrbysco.monstereggs.registry.EggPlacedFeatures;
+import com.mrbysco.monstereggs.worldgen.modifier.AddFeaturesBlacklistBiomeModifier;
+import net.minecraft.core.HolderGetter;
+import net.minecraft.core.HolderSet;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BiomeTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+import net.minecraftforge.common.world.BiomeModifier;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MonsterBiomeModifiers {
+
+	public static final ResourceKey<BiomeModifier> CAVE_SPIDER_HANGING_EGG = ResourceKey.create(ForgeRegistries.Keys.BIOME_MODIFIERS, new ResourceLocation(MonsterEggs.MOD_ID, "cave_spider_hanging_egg"));
+	public static final ResourceKey<BiomeModifier> CAVE_SPIDER_EGG = ResourceKey.create(ForgeRegistries.Keys.BIOME_MODIFIERS, new ResourceLocation(MonsterEggs.MOD_ID, "cave_spider_egg"));
+	public static final ResourceKey<BiomeModifier> CREEPER_HANGING_EGG = ResourceKey.create(ForgeRegistries.Keys.BIOME_MODIFIERS, new ResourceLocation(MonsterEggs.MOD_ID, "creeper_hanging_egg"));
+	public static final ResourceKey<BiomeModifier> CREEPER_EGG = ResourceKey.create(ForgeRegistries.Keys.BIOME_MODIFIERS, new ResourceLocation(MonsterEggs.MOD_ID, "creeper_egg"));
+	public static final ResourceKey<BiomeModifier> ENDERMAN_HANGING_EGG = ResourceKey.create(ForgeRegistries.Keys.BIOME_MODIFIERS, new ResourceLocation(MonsterEggs.MOD_ID, "enderman_hanging_egg"));
+	public static final ResourceKey<BiomeModifier> ENDERMAN_EGG = ResourceKey.create(ForgeRegistries.Keys.BIOME_MODIFIERS, new ResourceLocation(MonsterEggs.MOD_ID, "enderman_egg"));
+	public static final ResourceKey<BiomeModifier> SKELETON_HANGING_EGG = ResourceKey.create(ForgeRegistries.Keys.BIOME_MODIFIERS, new ResourceLocation(MonsterEggs.MOD_ID, "skeleton_hanging_egg"));
+	public static final ResourceKey<BiomeModifier> SKELETON_EGG = ResourceKey.create(ForgeRegistries.Keys.BIOME_MODIFIERS, new ResourceLocation(MonsterEggs.MOD_ID, "skeleton_egg"));
+	public static final ResourceKey<BiomeModifier> SPIDER_HANGING_EGG = ResourceKey.create(ForgeRegistries.Keys.BIOME_MODIFIERS, new ResourceLocation(MonsterEggs.MOD_ID, "spider_hanging_egg"));
+	public static final ResourceKey<BiomeModifier> SPIDER_EGG = ResourceKey.create(ForgeRegistries.Keys.BIOME_MODIFIERS, new ResourceLocation(MonsterEggs.MOD_ID, "spider_egg"));
+	public static final ResourceKey<BiomeModifier> ZOMBIE_HANGING_EGG = ResourceKey.create(ForgeRegistries.Keys.BIOME_MODIFIERS, new ResourceLocation(MonsterEggs.MOD_ID, "zombie_hanging_egg"));
+	public static final ResourceKey<BiomeModifier> ZOMBIE_EGG = ResourceKey.create(ForgeRegistries.Keys.BIOME_MODIFIERS, new ResourceLocation(MonsterEggs.MOD_ID, "zombie_egg"));
+
+	public static void bootstrap(BootstapContext<BiomeModifier> context) {
+		HolderGetter<Biome> biomeGetter = context.lookup(Registries.BIOME);
+		HolderGetter<PlacedFeature> placedGetter = context.lookup(Registries.PLACED_FEATURE);
+
+		List<TagKey<Biome>> overworld = List.of(BiomeTags.IS_OVERWORLD);
+		generateBiomeModifier(context, biomeGetter, placedGetter, CAVE_SPIDER_HANGING_EGG, EggPlacedFeatures.CAVE_SPIDER_HANGING_EGG,
+				overworld, List.of(BiomeTags.IS_NETHER, BiomeTags.IS_END), GenerationStep.Decoration.UNDERGROUND_DECORATION);
+		generateBiomeModifier(context, biomeGetter, placedGetter, CAVE_SPIDER_EGG, EggPlacedFeatures.CAVE_SPIDER_EGG,
+				overworld, List.of(BiomeTags.IS_NETHER, BiomeTags.IS_END), GenerationStep.Decoration.UNDERGROUND_DECORATION);
+		generateBiomeModifier(context, biomeGetter, placedGetter, CREEPER_HANGING_EGG, EggPlacedFeatures.CREEPER_HANGING_EGG,
+				overworld, List.of(BiomeTags.IS_NETHER, BiomeTags.IS_END), GenerationStep.Decoration.UNDERGROUND_DECORATION);
+		generateBiomeModifier(context, biomeGetter, placedGetter, CREEPER_EGG, EggPlacedFeatures.CREEPER_EGG,
+				overworld, List.of(BiomeTags.IS_NETHER, BiomeTags.IS_END), GenerationStep.Decoration.UNDERGROUND_DECORATION);
+		generateBiomeModifier(context, biomeGetter, placedGetter, ENDERMAN_HANGING_EGG, EggPlacedFeatures.ENDERMAN_HANGING_EGG,
+				overworld, List.of(BiomeTags.IS_NETHER, BiomeTags.IS_END), GenerationStep.Decoration.UNDERGROUND_DECORATION);
+		generateBiomeModifier(context, biomeGetter, placedGetter, ENDERMAN_EGG, EggPlacedFeatures.ENDERMAN_EGG,
+				overworld, List.of(BiomeTags.IS_NETHER, BiomeTags.IS_END), GenerationStep.Decoration.UNDERGROUND_DECORATION);
+		generateBiomeModifier(context, biomeGetter, placedGetter, SKELETON_HANGING_EGG, EggPlacedFeatures.SKELETON_HANGING_EGG,
+				overworld, List.of(BiomeTags.IS_NETHER, BiomeTags.IS_END), GenerationStep.Decoration.UNDERGROUND_DECORATION);
+		generateBiomeModifier(context, biomeGetter, placedGetter, SKELETON_EGG, EggPlacedFeatures.SKELETON_EGG,
+				overworld, List.of(BiomeTags.IS_NETHER, BiomeTags.IS_END), GenerationStep.Decoration.UNDERGROUND_DECORATION);
+		generateBiomeModifier(context, biomeGetter, placedGetter, SPIDER_HANGING_EGG, EggPlacedFeatures.SPIDER_HANGING_EGG,
+				overworld, List.of(BiomeTags.IS_NETHER, BiomeTags.IS_END), GenerationStep.Decoration.UNDERGROUND_DECORATION);
+		generateBiomeModifier(context, biomeGetter, placedGetter, SPIDER_EGG, EggPlacedFeatures.SPIDER_EGG,
+				overworld, List.of(BiomeTags.IS_NETHER, BiomeTags.IS_END), GenerationStep.Decoration.UNDERGROUND_DECORATION);
+		generateBiomeModifier(context, biomeGetter, placedGetter, ZOMBIE_HANGING_EGG, EggPlacedFeatures.ZOMBIE_HANGING_EGG,
+				overworld, List.of(BiomeTags.IS_NETHER, BiomeTags.IS_END), GenerationStep.Decoration.UNDERGROUND_DECORATION);
+		generateBiomeModifier(context, biomeGetter, placedGetter, ZOMBIE_EGG, EggPlacedFeatures.ZOMBIE_EGG,
+				overworld, List.of(BiomeTags.IS_NETHER, BiomeTags.IS_END), GenerationStep.Decoration.UNDERGROUND_DECORATION);
+	}
+
+
+	private static void generateBiomeModifier(BootstapContext<BiomeModifier> context,
+											  HolderGetter<Biome> biomeGetter,
+											  HolderGetter<PlacedFeature> placedGetter,
+											  ResourceKey<BiomeModifier> modifierKey,
+											  ResourceKey<PlacedFeature> placedKey,
+											  @NotNull List<TagKey<Biome>> tags,
+											  @Nullable List<TagKey<Biome>> blacklistTags,
+											  GenerationStep.Decoration decorationType) {
+		final List<HolderSet<Biome>> tagHolders = tags.stream()
+				.map(tag -> biomeGetter.getOrThrow(tag)).collect(Collectors.toList());
+		final List<HolderSet<Biome>> blacklistTagHolders = blacklistTags.isEmpty() ? List.of() : blacklistTags.stream()
+				.map(tag -> biomeGetter.getOrThrow(tag)).collect(Collectors.toList());
+		final BiomeModifier addFeature = new AddFeaturesBlacklistBiomeModifier(
+				tagHolders,
+				blacklistTagHolders,
+				HolderSet.direct(placedGetter.getOrThrow(placedKey)),
+				decorationType);
+		context.register(modifierKey, addFeature);
+	}
+}
